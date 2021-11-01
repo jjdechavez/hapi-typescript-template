2d24aae3c1d4e345709327dea4655d5f93dd612b
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-11-01 17:08:58 +0800

    Updated user validator - name and password field with minimum

--- src/api/users/user-validator.ts
+++ src/api/users/user-validator.ts
@@ -2,14 +2,14 @@ import Joi from 'joi';
 
 export const createUserModel = Joi.object().keys({
   username: Joi.string().trim().required(),
-  name: Joi.string().required(),
-  password: Joi.string().trim().required(),
+  name: Joi.string().min(3).required(),
+  password: Joi.string().min(6).trim().required(),
 });
 
 export const updateUserModel = Joi.object().keys({
   username: Joi.string().email().trim(),
-  name: Joi.string(),
-  password: Joi.string().trim(),
+  name: Joi.string().min(3),
+  password: Joi.string().min(6).trim(),
 });
 
 export const loginUserModel = Joi.object().keys({
