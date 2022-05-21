93d02f62f507ff8fd01e5687a42bb9cf72440bd1
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-05-21 11:12:13 +0800

    Added test should login user

--- test/users/users-controller.ts
+++ test/users/users-controller.ts
@@ -13,6 +13,15 @@ const serverConfig = Configs.getServerConfig();
 const lab = Lab.script();
 export {lab};
 
+type UserTest = {
+  name: string;
+  username: string;
+  password: string;
+  token: string;
+};
+
+const users: UserTest[] = [];
+
 lab.experiment('UserController Test', () => {
   let server: Hapi.Server;
 
@@ -36,6 +45,21 @@ lab.experiment('UserController Test', () => {
       const response = JSON.parse(res.payload);
       expect(res.statusCode).to.equal(201);
       expect(response.token).to.not.null();
+
+      users.push({...user, token: response.token});
     });
+  });
+
+  lab.test('Should login user', async () => {
+    const {username, password} = users[0];
+    const res = await server.inject({
+      method: 'POST',
+      url: serverConfig.routePrefix + '/users/login',
+      payload: {username, password},
+    });
+
+    const response = JSON.parse(res.payload);
+    expect(res.statusCode).to.equal(200);
+    expect(response.token).to.not.null();
   });
 });
