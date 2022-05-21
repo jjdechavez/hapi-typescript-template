ff0418d6e745f56e0b0d4051588e83b16449ca21
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-05-21 12:29:22 +0800

    Initial blog controller test

---
+++ test/blogs/blog-controller.ts
@@ -0,0 +1,76 @@
+import Hapi from '@hapi/hapi';
+import {expect} from '@hapi/code';
+import * as Lab from '@hapi/lab';
+import * as Configs from '../../src/configurations';
+import * as Server from '../../src/server';
+import * as Database from '../../src/database';
+import {UserTest} from '../users/users-controller';
+import blogData from './blogs.fixtures.json';
+import userData from '../users/users.fixtures.json';
+
+const configDb = Configs.getDatabaseConfig();
+const database = Database.init(configDb);
+const serverConfig = Configs.getServerConfig();
+
+const lab = Lab.script();
+export {lab};
+
+type Blog = {
+  name: string;
+  description: string;
+};
+
+const users: UserTest[] = [];
+const blogs: Blog[] = [];
+
+lab.experiment('BlogController Test', () => {
+  let server: Hapi.Server;
+
+  lab.before(async () => {
+    server = await Server.init(serverConfig, database);
+    await database.userModel.deleteMany({});
+    await database.blogModel.deleteMany({});
+  });
+
+  lab.after(async () => {
+    await server.stop();
+  });
+
+  userData.forEach((user: any) => {
+    lab.test(`Should create user: ${user.name}}`, async () => {
+      const res = await server.inject({
+        method: 'POST',
+        url: serverConfig.routePrefix + '/users',
+        payload: user,
+      });
+
+      const response = JSON.parse(res.payload);
+      expect(res.statusCode).to.equal(201);
+      expect(response.token).to.not.null();
+
+      users.push({...user, token: response.token});
+    });
+  });
+
+  blogData.forEach((blog: Blog, index: number) => {
+    lab.test(`Should create blog: ${blog.name}`, async () => {
+      const res = await server.inject({
+        method: 'POST',
+        url: serverConfig.routePrefix + '/blogs',
+        payload: blog,
+        headers: {
+          authorization: users[index].token,
+        },
+      });
+
+      const response = JSON.parse(res.payload);
+      expect(res.statusCode).to.equal(201);
+      expect(response.name).to.equal(blog.name);
+      expect(response.description).to.equal(blog.description);
+      expect(response).to.include('createdAt');
+      expect(response).to.include('updatedAt');
+
+      blogs.push(blog);
+    });
+  });
+});
